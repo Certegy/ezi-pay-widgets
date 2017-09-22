a4dede99b552159bf8fb312abf44f700bc362d05
tao wang <hellotaotao@gmail.com>
2017-09-22 12:02:30 +0930

    change s3 buckets address as cname is not done yet

--- src/au/config.ts
+++ src/au/config.ts
@@ -4,8 +4,8 @@
     public static infoModalId = 'oxipay-modal-info';
     public static priceInfoModalId = 'oxipay-modal-price-info';
     public static scriptId = 'script[id^=oxipay-banner]';
-    public static moreInfoUrl = 'https://widgets.certegyezipay.com.au/content/html/ExternalModal.html';
-    public static registerInterestUrl = 'https://widgets.certegyezipay.com.au/content/html/Signup.html';
-    public static baseContentUrl = 'https://widgets.certegyezipay.com.au/';
-    public static priceInfoUrl = 'https://widgets.certegyezipay.com.au/content/html/PriceInfo.html';
+    public static moreInfoUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/ExternalModal.html';
+    public static registerInterestUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/Signup.html';
+    public static baseContentUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/';
+    public static priceInfoUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/PriceInfo.html';
 }
--- src/nz/config.ts
+++ src/nz/config.ts
@@ -3,7 +3,7 @@
     public static infoModalId = 'oxipay-modal-info';
     public static priceInfoModalId = 'oxipay-modal-price-info';
     public static scriptId = 'script[id^=oxipay-banner]';
-    public static moreInfoUrl = 'https://widgets.certegyezipay.co.nz/content/html/ExternalModal.html';    
+    public static moreInfoUrl = 'https://widgets.certegyezipay.co.nz.s3-website-ap-southeast-2.amazonaws.com/content/html/ExternalModal.html';    
     public static baseContentUrl = 'https://images.oxipay.com.au';
-    public static priceInfoUrl = 'https://widgets.certegyezipay.co.nz/content/html/PriceInfo.html';
+    public static priceInfoUrl = 'https://widgets.certegyezipay.co.nz.s3-website-ap-southeast-2.amazonaws.com/content/html/PriceInfo.html';
 }
