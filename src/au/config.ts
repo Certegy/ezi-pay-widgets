f8138a9c724a8bce0a5464d7c4f9af89e624bfc9
tao wang <hellotaotao@gmail.com>
2017-09-22 13:24:23 +0930

    https -> http as s3 does not respond to https

--- src/au/config.ts
+++ src/au/config.ts
@@ -4,8 +4,8 @@
     public static infoModalId = 'oxipay-modal-info';
     public static priceInfoModalId = 'oxipay-modal-price-info';
     public static scriptId = 'script[id^=oxipay-banner]';
-    public static moreInfoUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/ExternalModal.html';
-    public static registerInterestUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/Signup.html';
-    public static baseContentUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/';
-    public static priceInfoUrl = 'https://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/PriceInfo.html';
+    public static moreInfoUrl = 'http://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/ExternalModal.html';
+    public static registerInterestUrl = 'http://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/Signup.html';
+    public static baseContentUrl = 'http://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com';
+    public static priceInfoUrl = 'http://widgets.certegyezipay.com.au.s3-website-ap-southeast-2.amazonaws.com/content/html/PriceInfo.html';
 }
